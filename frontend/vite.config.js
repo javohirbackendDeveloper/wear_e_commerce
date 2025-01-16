bd360b596527866738a746776b0ba11ce31db32c
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-01-16 08:04:41 +0500

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist", // Build fayllari uchun chiqish papkasi
-  },
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000", // API uchun proxy
+        target: "http://localhost:4000",
       },
     },
   },
-  publicDir: "public", // Statik fayllar uchun public papkasi
 });
